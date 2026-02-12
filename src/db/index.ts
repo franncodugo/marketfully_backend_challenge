b2d2bb378669449550fa661d04f407b993f2ef65
Dugo, Franco <FDugo@HudsonInsGroup.com>
2026-02-12 00:38:28 -0300

    Adding gitignore. Create basic folder structure. Adding tsconfig for typescript. Adding logic for the DB (propierties table & cache table for zip). Adding script section in package.json.

---
+++ src/db/index.ts
@@ -0,0 +1,48 @@
+import Database from 'better-sqlite3';
+import path from 'path';
+
+// db file creation.
+const dbPath = path.resolve(__dirname, '../../houses.db');
+const db = new Database(dbPath);
+
+// opt.
+db.pragma('journal_mode = WAL');
+
+export default db;
+
+export const initDb = () => {
+  // Propierties table
+  db.exec(`
+    CREATE TABLE IF NOT EXISTS properties (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      status TEXT,
+      price REAL,
+      bed INTEGER,
+      bath INTEGER,
+      acre_lot REAL,
+      full_address TEXT,
+      street TEXT,
+      city TEXT,
+      state TEXT,
+      zip_code TEXT,
+      house_size REAL,
+      sold_date TEXT,
+      state_code TEXT,
+      price_per_sq_ft REAL,
+      price_per_acre REAL
+    );
+    
+    CREATE INDEX IF NOT EXISTS idx_zip ON properties(zip_code);
+  `);
+
+  // Cache table for zip codes
+  db.exec(`
+    CREATE TABLE IF NOT EXISTS zip_cache (
+      zip_code TEXT PRIMARY KEY,
+      data TEXT,
+      updated_at DATETIME DEFAULT CURRENT_TIMESTAMP
+    );
+  `);
+
+  console.log('✅ DB initialized.');
+};
